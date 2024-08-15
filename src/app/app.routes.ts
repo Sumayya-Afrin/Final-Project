b4088b8ceeeee111531b86e546c4aeb365c0bf75
Sumayya-Afrin <sumayyafrin.mohammad@proclink.com>
2024-08-15 13:45:17 +0530

    home page is done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { CraftsComponent } from './crafts/crafts.component';
+
+export const routes: Routes = [{ path: 'Crafts', component: CraftsComponent }];
---
+++ src/app/crafts/crafts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'app-crafts',
+  standalone: true,
+  imports: [],
+  templateUrl: './crafts.component.html',
+  styleUrl: './crafts.component.scss',
+})
+export class CraftsComponent {}
